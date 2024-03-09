1a847dffcaf9a7d514525a9d6097b43505adf706
Starboy <samskywalker001@gmail.com>
2024-03-09 23:29:09 +0530

    60sec

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ Purpose: Timer Application
 Stardate: 12024.03.07.1047
 */
 /* TODOs:
-- Custom timer input
+- Timer choices
 - Sound on timer completion
 - Timer completion message
-- Black Panther theme
+- Black Panther theme: ftimer - Fancy Timer
 
 */
 
@@ -16,10 +16,37 @@ import './App.css'
 import Timer from './components/Timer'
 import 'tachyons'
 
+let hours = 0
+let minutes = 0
+let seconds = 60
+
+// function TimerChoices(){
+//     return(
+//         <div>
+//             <h1 mr3 red >Pick your poison...</h1>
+//             <div>
+//                 <button onClick={handleClick60s}>60sec</button>
+//                 <button onClick={handleClick}>2min</button>
+//                 <button onClick={handleClick}>3min</button>
+//                 <button onClick={handleClick}>5min</button>
+//                 <button onClick={handleClick}>10min</button>
+//                 <button onClick={handleClick}>15min</button>
+//                 <button onClick={handleClick}>30min</button>
+//                 <button onClick={handleClick}>45min</button>
+//                 <button onClick={handleClick}>60min</button>
+//                 <button onClick={handleClick}>90min</button>
+//                 <button onClick={handleClick}>180min</button>
+//             </div>
+//         </div>
+//     )
+// }
+
+
 function App() {
   return (
     <>
-      <Timer hr={0} min={2} sec={0}></Timer>
+        {/* <TimerChoices></TimerChoices> */}
+        <Timer hr={hours} min={minutes} sec={seconds}></Timer>
     </>
   )
 }
